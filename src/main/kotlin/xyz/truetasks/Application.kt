b87a90100688096b1926d73638202afa1b7e24f5
Eric Freitas <freitaseric@proton.me>
2024-12-28 14:21:26 -0400

    initial Commit

---
+++ src/main/kotlin/xyz/truetasks/Application.kt
@@ -0,0 +1,12 @@
+package xyz.truetasks
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureDatabases()
+    configureTemplating()
+}
---
+++ src/main/kotlin/xyz/truetasks/Templating.kt
@@ -0,0 +1,24 @@
+package xyz.truetasks
+
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.thymeleaf.*
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
+
+fun Application.configureTemplating() {
+    install(Thymeleaf) {
+        setTemplateResolver(ClassLoaderTemplateResolver().apply {
+            prefix = "templates/thymeleaf/"
+            suffix = ".html"
+            characterEncoding = "utf-8"
+        })
+    }
+    routing {
+        staticResources("/static", "static")
+        get("/") {
+            call.respond(ThymeleafContent("index", emptyMap()))
+        }
+    }
+}
